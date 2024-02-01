5d8a3f2497becfec712f49e67a5cd3111b50e274
Infinity <isd6542@gmail.com>
2024-02-01 17:16:55 +1100

    Fixed critical error

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-let clientSupportsScroll = (CSS.supports("animation-timeline", "scroll()"));
-
-if (clientSupportsScroll == false) {
-    window.alert("Whoops! It looks like your browser doesn't support a feature which this site requires. Redirecting...");
-    window.location.href = ""; // TODO: implement this
-}
